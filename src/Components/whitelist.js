9f601e712e31980b07888295e6be02a24ae30c20
cartalegault <62150569+cartalegault@users.noreply.github.com>
2022-05-14 20:31:13 -0700

    fix build errors

--- src/Components/whitelist.js
+++ src/Components/whitelist.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react'
-import Button from '@mui/material/Button';
+import React from 'react'
 //import { BrowserRouter as Router, Route, useHistory, Link, withRouter, Switch } from "react-router-dom";
 
 
