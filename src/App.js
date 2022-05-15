e9827c6ecb849b28ff2eccbbdc95686407a4e95e
cartalegault <62150569+cartalegault@users.noreply.github.com>
2022-05-14 20:26:43 -0700

    improve terminal + new categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-//import 'terminal-in-react/lib/css/index.css';
 import Terminal from 'react-bash';
+import WhiteList from './Components/whitelist.js'
+import Container from '@mui/material/Container';
 //import mmlogo from './Components/metamask-fox.svg'
 import * as React from 'react';
-//import Button from '@mui/material/Button';
-
-
 
 
 function App() {
@@ -14,16 +12,22 @@ const extensions = {
     sudo: {
         exec: ({ structure, history, cwd }) => {
             return { structure, cwd,
-                history: history.concat({ value: 'ERROR: Satoshi not found' }),
+                history: history.concat({ value: 'FATAL ERROR: Satoshi not found. Exit Code: 128+n' }),
             };
         },
     },
 };
 
-
+const styles = {
+    header : {padding: '0px 0px 0',},
+    prefix : {color: '#57d51f',},
+    redCircle : {backgroundColor: '#000',},
+    greenCircle : {backgroundColor: '#000',},
+    yellowCircle : {backgroundColor: '#000',},
+};
 
 const history = [
-    { value: 'Connecting to the blockchain...' },
+    { value: 'Connecting to 0x321162Cd933E2Be498Cd2267a90534A804051b11' },
     { value: '.....' },
     { value: '.....' },
     { value: 'Success. Initializing protocol farm' },
@@ -35,46 +39,39 @@ const history = [
 
 const structure = {
     '.hidden': {
-        GorillaDEX: { content: 'The is the content for file1 in the <.hidden> directory.' },
+        GorillaDEX: { content: '.' },
         ClownCoin: { content: 'CLWN is a stableclown that randomly changes its value every 10 minutes' },
         dir2: {
-            file: { content: ' http://oncyber.io/schrutefarms' },
+            file: { content:'http://oncyber.io/schrutefarms' },
         },
         '.secrets': { content: 'Clownana' },
         '.origins': { content: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa' },
-
+        '.submit': { content: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa' },
     },
-    public: {
+    protocols: {
         JetProtocol: { content: 'a decentralized protocol for borrowing and lending on the Solana Blockchain' },
         Oxsplits: { content: '0xSplits is an open-source, audited, and non-upgradeable protocol for efficiently splitting onchain income. Whenever a Split receives funds, each recipient gets their share. Simple enough for friends, secure enough for anons.' },
         Syndicate: { content: 'Syndicate is revolutionizing investing by changing how communities and capital work together through accessible, effortless, and social web3 technologies.' },
     },
-    'README.md': { content: 'protocol-farm helps new protocols get discovered to ensure that WAGMI. Sign up below to get early access. Submit your protocol to be featured if you can figure out how....' },
+    topLiquidityProviders: {
+        comingSoon: { content: '$$$$' },
+    },
+    'README.md': { content: 'protocol-farm helps new protocols get discovered to ensure that WAGMI. Sign up on the whitelist to get early access. Submit your protocol to be featured if you can figure out how....' },
+     'Whitelist.md': { content: 'Access Granted: 3 12 15 23 14'},
 };
+
+//console.log(this.state)
 
   return (
 
-
-
     <div className="App">
-
       <header className="App-header">
+        <div></div>
+      <Container maxWidth="sm" sx={{ maxHeight: '75%' }}>
+        <Terminal styles={styles} history={history} extensions={extensions} structure={structure} theme= 'dark' prefix= 'hacker@1A1zP1' />
+        </Container>
+        <WhiteList history={history} />
 
-
-
-
-        <Terminal history={history} extensions={extensions} structure={structure} theme= 'dark' prefix= 'hacker@1A1zP1' />
-        <p>
-         Coming Soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://forms.gle/k8L4k7EG5T7Q7sG2A"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Whitelist
-        </a>
       </header>
     </div>
   );
---
+++ src/Components/whitelist.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect, useContext} from 'react'
+import Button from '@mui/material/Button';
+//import { BrowserRouter as Router, Route, useHistory, Link, withRouter, Switch } from "react-router-dom";
+
+
+function WhiteList(props) {
+       console.log(props.history)
+      // const [value, setValue] = useState("empty");
+       //useEffect(() => { setValue(props) }, [props]);
+
+
+
+    if (props.history.find(hist => hist.value === 'Access Granted: 3 12 15 23 14')
+) {
+    return (
+
+     <a
+
+          className="App-link"
+          href="https://forms.gle/k8L4k7EG5T7Q7sG2A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Whitelist
+        </a>
+
+)}
+    return <p>Coming soon</p>;
+
+}
+
+export default WhiteList;
+
+    //TODO: add routing to another page for bear upload
+
+
+
+
