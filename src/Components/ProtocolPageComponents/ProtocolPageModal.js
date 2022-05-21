cdc5ec99a301dc4fbafb858e84739b814871f27a
cartalegault <62150569+cartalegault@users.noreply.github.com>
2022-05-20 23:07:42 -0700

    Add Modal + data via CSV
    -refactored protocols page to read from a CSV
    -added modal to show more info about each protocol
    -moved some styling higher up. MUI button styling is now broken though

---
+++ src/Components/ProtocolPageComponents/ProtocolPageModal.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '75%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  color: '#FFB000',
+};
+
+export default function BasicModal(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+console.log(props.modalInfo)
+    if (props.modalInfo.wen_token === '' ){
+    props.modalInfo.wen_token =  "¯\\_(ツ)_/¯"
+    }
+  return (
+    <div>
+      <Button onClick={handleOpen}>View</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+            {props.modalInfo.protocol_name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {props.modalInfo.long_description}
+          </Typography>
+           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Contract Created Date: {props.modalInfo.first_contract_created_date}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Contract Interactions: {props.modalInfo.contract_interactions}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Whitepaper/Docs: {props.modalInfo.whitepaper_link}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Discord: {props.modalInfo.discord}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Twitter: {props.modalInfo.twitter}
+          </Typography>
+           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Founder(s): {props.modalInfo.founders}
+          </Typography>
+           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Wen Token: {props.modalInfo.wen_token}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/Pages/Protocols.js
+++ src/Pages/Protocols.js
@@ -1,19 +1,17 @@
 import React from "react";
-import ProtocolCardList from '../Components/ProtocolPageComponents/ProtocolCardList.js'
+import Button from '@mui/material/Button';
+import ProtocolCardList from '../Components/ProtocolPageComponents/ProtocolCardList2.js';
 //Functional Component
 const Protocols = () => {
   return (
     <div className="App-link">
-      <h1>Coming Soon</h1>
+      <h1>Hottest Protocols</h1>
       <ProtocolCardList/>
-       <a
-          className="App-link"
-          href="https://forms.gle/FGdKocFmBX6zJZox5"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Submit A Protocol
-        </a>
+      <p></p>
+ <Button variant="outlined" href="https://forms.gle/FGdKocFmBX6zJZox5">
+        Submit A Protocol
+        </Button>
+
     </div>
   );
 };
